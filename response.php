b45d6c43606946a84b3f5b3489553d5c8e1329d0
LyleRomero <lylearomero@gmail.com>
2018-01-20 19:13:59 -0500

    add key response

--- response.php
+++ response.php
@@ -1,17 +1,29 @@
 <?php 
     $request = $_GET;
     $requestToJSON = json_encode($request) . "<br>";
+
     // echo $requestToJSON;
-    $file_dummy = fopen("dummy.json", "r") or die ("failed open file");
-    $data = fread($file_dummy, filesize("dummy.json"));
-    // echo $data . "<br>";
-    /* $data_array = json_decode($data);
-    var_dump($data_array);
-    echo "<br><br>";
-    for($i = 0; $i < count($data_array); $i++) { 
-		foreach($data_array[$i] as $key => $value) {
-			echo "{$key} -> {$value} <br>";
-		}
-	} */
-	echo $data;
+
+    $key = $_GET["key"];
+
+    if($key == "ABC") {
+	$file_dummy = fopen("dummy.json", "r") or die ("failed open file");
+    	$data = fread($file_dummy, filesize("dummy.json"));
+
+	// echo $data . "<br>";
+    	/* $data_array = json_decode($data);
+    	var_dump($data_array);
+	echo "<br><br>";
+    	for($i = 0; $i < count($data_array); $i++) { 
+	    foreach($data_array[$i] as $key => $value) {
+		echo "{$key} -> {$value} <br>";
+	    }
+        } */
+        echo $data;
+    } else {
+	header("HTTP/1.0 404 Not Found");
+        exit;
+    }
+
+    
 ?>
